4d87ff6c64c5a9d520546bab40de92c8380dfe5c
daeneil lopernes <daeneillopernes@gmail.com>
2024-02-02 14:23:49 +0000

    codes subject to be modifield as needed

---
+++ src/script.js
@@ -0,0 +1,9 @@
+// Import React and other dependencies
+// ... (same as your provided code)
+
+ReactDOM.render(<CCTVVideoPlayer videoId={'your-video-id'} metadata={{
+    timestamp: '2023-11-21 15:30:00',
+    cameraName: 'Front Entrance',
+    cameraLocation: 'Main Lobby',
+  }} />, document.getElementById('root'));
+  
